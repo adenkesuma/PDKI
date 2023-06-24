fa5cdc6ac2f26ad8e2cb6dc4b99ccca1d8954844
adenkesuma <adenfdfd10@gmail.com>
2023-06-24 16:46:50 +0700

    add conference data :error

--- src/app/conference/page.tsx
+++ src/app/conference/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import { TbArrowUpRight } from "react-icons/tb"
-import { filterDropdownConference } from "@/utils/constant"
-import { ConferenceProps } from "@/utils/interface"
 import Link from "next/link"
-import Image from "next/image"
 import Header from "@/components/header"
+import Image from "next/image"
+import { ConferenceProps } from "@/utils/interface"
+
 
 async function fetchConference() {
   const res = await fetch('http://localhost:8080/api/route/conference')
@@ -21,45 +21,45 @@ async function fetchConference() {
 const Conference = async () => {
   const getFetchConference = await fetchConference()
 
-
   return (
     <>
       <main className="container px-4 sm:px-0 mx-auto">
-        <Header heading="PDKI" subheading="Konferensi dan Acara" /> 
+        {/* header dari halaman berita */}
+        <Header heading="PDKI" subheading="Konferensi dan Acara" />
 
         <section className="my-12">
-          <h2 className="font-semibold text-[30px] mb-4">Konferensi</h2> 
+          <h2 className="font-semibold text-[30px] mb-4">Konferensi</h2>
+          <input type="text" />
 
-          {/* conference */}
+          {/* news */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             {getFetchConference.data.map((item : ConferenceProps) => (
-              <div key={item.id}>
-                <figure className="h-[160px] relative block overflow-hidden rounded-tl-2xl rounded-tr-2xl">
-                  <Image 
-                    width={300}
-                    height={300}
-                    className="duration-100 object-cover bg-cover h-full hover:scale-110 w-full rounded-tr-2xl rounded-tl-2xl"
-                    src={item.image}
-                    alt="news 1"
-                  />
-                  <div className="absolute top-5 right-5 p-2 rounded-[50%] bg-white">
-                    <Link href="/news/:id">
-                      hahahahhaha
-                      <TbArrowUpRight className="w-[24px] h-[24px] text-[#274698]"/>
-                    </Link>
+               <div key={item.id}>
+                  <figure className="h-[160px] relative block overflow-hidden rounded-tl-2xl rounded-tr-2xl">
+                    <Image 
+                      width={300}
+                      height={300}
+                      className="duration-100 object-cover bg-cover h-full hover:scale-110 w-full rounded-tr-2xl rounded-tl-2xl"
+                      src={item.image}
+                      alt="news 1"
+                    />
+                    <div className="absolute top-5 right-5 p-2 rounded-[50%] bg-white">
+                      <Link href="/news/:id">
+                        <TbArrowUpRight className="w-[24px] h-[24px] text-[#274698]"/>
+                      </Link>
+                    </div>
+                  </figure>
+                  <div className="flex flex-col justify-between p-6 bg-[#274698] rounded-bl-2xl overflow-hidden rounded-br-2xl h-[140px]">
+                    <div>
+                      <h4 className="text-white font-semibold text-[16px] text-ellipsis overflow-hidden whitespace-nowrap">{item.title}</h4>
+                      <p className="text-gray-300 text-[14px] font-medium text-ellipsis overflow-hidden whitespace-nowrap">{item.description}</p>
+                    </div>
+                    {/* <div className="mt-4 flex justify-between items-center">
+                      <span className="text-[14px] text-gray-100 font-medium">{item.publishedDate}</span>
+                      <span className="text-[14px] text-gray-100 font-medium">{item.tags}</span>
+                    </div> */}
                   </div>
-                </figure>
-                <div className="flex flex-col justify-between p-6 bg-[#274698] rounded-bl-2xl overflow-hidden rounded-br-2xl h-[140px]">
-                  <div>
-                    <h4 className="text-white font-semibold text-[16px] text-ellipsis overflow-hidden whitespace-nowrap">{item.title}</h4>
-                    <p className="text-gray-300 text-[14px] font-medium text-ellipsis overflow-hidden whitespace-nowrap">{item.description}</p>
-                  </div>
-                  <div className="mt-4 flex justify-between items-center">
-                    <span className="text-[14px] text-gray-100 font-medium">{item.location}</span>
-                    <span className="text-[14px] text-gray-100 font-medium">{item.organizer}</span>
-                  </div>
-                </div>
-              </div> 
+                </div>  
             ))} 
           </div>         
         </section>
